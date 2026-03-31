5f034bfa5346c9a54c776903c13f8d5ac2340f86
ghimirebabin07 <240107babin@cosmoscollege.edu.np>
2026-03-31 17:05:07 +0545

    Tower Of hanoi

--- Tower of Hanoi.c
+++ Tower of Hanoi.c
@@ -1,21 +1,21 @@
-//TOH 
-#include <stdio.h> 
-void hanoi(int n, char source, char destination, char auxiliary) 
-{ 
-if (n == 1) 
-{ 
-printf("Move disk 1 from %c to %c\n", source, destination); 
-return; 
-} 
-hanoi(n - 1, source, auxiliary, destination); 
-printf("Move disk %d from %c to %c\n", n, source, destination); 
-hanoi(n - 1, auxiliary, destination, source); 
-} 
-int main() 
-{ 
-int n; 
-printf("Enter number of disks: "); 
-scanf("%d", &n); 
-hanoi(n, 'A', 'C', 'B'); 
-return 0; 
+#include <stdio.h>
+
+void tower(int n, char source, char aux, char dest) {
+    if (n == 1) {
+        printf("Move disk 1 from %c to %c\n", source, dest);
+    } else {
+        tower(n-1, source, dest, aux);
+        printf("Move disk %d from %c to %c\n", n, source, dest);
+        tower(n-1, aux, source, dest);
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter number of disks: ");
+    scanf("%d", &n);
+
+    tower(n, 'A', 'B', 'C');
+
+    return 0;
 }
